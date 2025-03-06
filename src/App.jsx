6e1f0f1457525596d484feefed6adad86ab80e04
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-03-06 19:08:44 +0530

    develop Home page futhermore

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hearder from './components/Header'
 
 const App = () => {
   return (
-    <div>
-      lakshan sachintha munasinghe 
+    <div className='w-full overflow-hidden'>
+      <Hearder/>
     </div>
   )
 }
