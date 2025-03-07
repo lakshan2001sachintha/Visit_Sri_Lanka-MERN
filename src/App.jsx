3e8cf7b8df9503c6017fc06fdfca878cf7f1d16e
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-03-07 21:14:08 +0530

    create about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hearder from './components/Header'
+import About from './components/About'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Hearder/>
+      <About/>
     </div>
   )
 }
