506c3606d490e9d1d86373d65c973705cc1b920f
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-03-08 22:36:16 +0530

    create project part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hearder from './components/Header'
 import About from './components/About'
+import Projects from './components/Projects'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Hearder/>
       <About/>
+      <Projects/>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
       <div class='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
         <img src={assets.logo} alt=""/>
         <ul className='hidden md:flex gap-7 text-white'>
-            <a href='#Header' className='cursor-pointer hover:text-gray-400'>Home</a>
-            <a href='#Header' className='cursor-pointer hover:text-gray-400'>About</a>
-            <a href='#Header' className='cursor-pointer hover:text-gray-400'>Projects</a>
-            <a href='#Header' className='cursor-pointer hover:text-gray-400'>Testmonials</a>
+            <a href='#Home' className='cursor-pointer hover:text-gray-400'>Home</a>
+            <a href='#About' className='cursor-pointer hover:text-gray-400'>About</a>
+            <a href='#Projects' className='cursor-pointer hover:text-gray-400'>Projects</a>
+            <a href='#Testmonials' className='cursor-pointer hover:text-gray-400'>Testmonials</a>
         </ul>
            <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
            <img src={assets.menu_icon} className='md:hidden w-7 cursor-pointer' alt=''/>
