b223f4042b89cb47291b990191ae045e4f926013
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-04-19 20:31:37 +0530

    TGone page

--- FrontEnd/src/components/TGone.jsx
+++ FrontEnd/src/components/TGone.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import lanka from '../assets/lanka.png';
 
 function TGone() {
   return (
-    <div>
-      
+    <div className="relative w-[500px] h-[600px] mx-auto">
+      <img src={lanka} alt="Sri Lanka" className="w-full h-full object-contain" /> 
+      <div>
+          <div className="absolute right-[65%] top-[32%] w-1/2 h-1 bg-yellow-400 rounded-full"></div>
+          <div className="absolute right-[49.5%] top-[32%] w-20 h-1 bg-yellow-400 rounded-full transform rotate-45 origin-left"></div>
+      </div>
     </div>
   )
 }
--- FrontEnd/src/components/TGuide.jsx
+++ FrontEnd/src/components/TGuide.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Team from './Team'; 
+import TGone from './TGone';
 
 function TGuide() {
   return (
     <div className="container mx-auto p-10">
       <h1 className="text-4xl font-bold mb-6">Travel Guide</h1>
+      <TGone/>
       <Team />
     </div>
   );
